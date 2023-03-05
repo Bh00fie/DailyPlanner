a241e94e9eb32e11c8118ca306430ede340db902
Bh00fie <Bh00fie@gmail.com>
2023-03-05 22:09:54 +0000

    Added Function to check local storage for saved events and show information if stored

--- script.js
+++ script.js
@@ -2,3 +2,22 @@
 var container = $('.container');
 var timeBlocks = $('.time-block');
 var saveButton = $('.saveBtn i');
+
+// Function to check local storage for saved events and show information if stored
+function localStorageEvents() {
+  var savedEvent = JSON.parse(localStorage.getItem("saved"));
+  if (savedEvent !== null) {
+    timeBlocks.each(function() {
+      var description = $(this).find('.description textarea');
+      var selectedTime = parseInt($(this).attr("id"));
+      for (var i = 0; i < savedEvent.length; i++) {
+        if (savedEvent[i].hour == selectedTime) {
+          description.val(savedEvent[i].text);
+          break;
+        }
+      }
+    });
+  }
+}
+
+localStorageEvents();
